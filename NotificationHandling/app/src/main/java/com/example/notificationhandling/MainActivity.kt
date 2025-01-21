26f2493ac01ecdbd02d0bd6dce4ca130f08b8ae6
DroidMystic <sabirkhan786ddn@gmail.com>
2025-01-21 23:51:14 +0530

    inc

---
+++ NotificationHandling/app/src/main/java/com/example/notificationhandling/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.notificationhandling
+
+import android.Manifest
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import com.example.notificationhandling.ui.theme.NotificationHandlingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                699
+            )
+        }
+
+        val intent = Intent(this, CounterReceiver::class.java)
+        intent.action = CounterNotification.CounterActions.START.name
+        setContent {
+            NotificationHandlingTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+
+                    Button(
+                        onClick = {
+                            sendBroadcast(intent)
+                        }
+                    ){
+                        Text(text="Show Counter Notification")
+                    }
+                }
+            }
+        }
+    }
+}
